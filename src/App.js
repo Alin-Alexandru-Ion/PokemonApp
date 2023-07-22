e358b1670d67abdee5e1e409bdf9aac482c07eb8
Alin-Alexandru-Ion <ionalinalexandru@gmail.com>
2023-07-22 22:39:55 +0200

    Added basic functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import PokemonList from "./PokemonList";
+import axios from "axios";
+import Pagination from "./Pagination";
 
 function App() {
-  const [pokemon, setPokemon] = useState(["pikachu", "meow"])
+  const [pokemon, setPokemon] = useState([])
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [nextPageUrl, setNextPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [prevPageUrl, setPrevPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setLoading(true)
+    let cancel 
+    axios.get(currentPageUrl, {
+        cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(res => {
+      setLoading(false)
+      setNextPageUrl(res.data.next)
+      setPrevPageUrl(res.data.previous)
+      setPokemon(res.data.results.map(p => p.name ))
+  })
+
+  return () => cancel.cancel
+
+  }, [currentPageUrl])
+
+  function accessNextPage() {
+    setCurrentPageUrl(nextPageUrl)
+  }
+
+  function accessPrevPage() {
+    setCurrentPageUrl(prevPageUrl)
+  }
+
+  if (loading) return "loading..."
+
   return (
-    <PokemonList pokemon = {pokemon} />
+    <>
+      <PokemonList pokemon={pokemon} />
+      <Pagination 
+        accessNextPage={accessNextPage}
+        accessPrevPage={accessPrevPage}
+      />
+    </>
   );
 }
 
--- src/PokemonList.js
+++ src/PokemonList.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-export default function PokemonList( {pokemon}) {
+export default function PokemonList({pokemon}) {
+
+    if (!pokemon)
+    {
+        return null
+    }
+
   return (
     <div>
-        {pokemon.map(p => (
+        {pokemon?.map(p => (
             <div key={p}> {p} </div>
         ) )}
     </div>
